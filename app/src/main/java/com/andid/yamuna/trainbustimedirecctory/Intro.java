6453612f0967d7ecf01e5db504ad9ca30707768c
Yamuna1996d <yamunag19@gmail.com>
2019-03-05 15:48:14 +0530

    intro

---
+++ app/src/main/java/com/andid/yamuna/trainbustimedirecctory/Intro.java
@@ -0,0 +1,41 @@
+package com.andid.yamuna.trainbustimedirecctory;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Intro extends AppCompatActivity {
+private TextView tv;
+private ImageView im;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        tv=(TextView)findViewById(R.id.te);
+        im=(ImageView)findViewById(R.id.img);
+        Animation animation=AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tv.startAnimation(animation);
+        im.startAnimation(animation);
+        final Intent i=new Intent(getApplicationContext(),AdminLogin.class);
+
+        Thread timer=new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }
+                catch (InterruptedException i){
+                    i.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
